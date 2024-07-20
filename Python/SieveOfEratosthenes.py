23d280c375d345eeb5d6101e0c1cf1ef30151985
Pranjal Asthana <92260070+PranjalAsthana@users.noreply.github.com>
2024-07-20 19:18:49 +0530

    Update SieveOfEratosthenes.py

--- Python/SieveOfEratosthenes.py
+++ Python/SieveOfEratosthenes.py
@@ -4,7 +4,7 @@ for i in range(2,n+1):
     if(sieve[i]):
         # for j in range(i,(n+2)//i):
         #     sieve[i*j] = 0
-        for j in range(2*i,n+1,i):
+        for j in range(i*i,n+1,i):
             sieve[j] = 0
 for i in range(2,n+1):
     if sieve[i]==1:
