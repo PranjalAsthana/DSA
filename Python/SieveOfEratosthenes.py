65beca4c8e97c6edce3d8c2dc20cfcf73d5fa737
Pranjal Asthana <92260070+PranjalAsthana@users.noreply.github.com>
2024-07-20 19:16:05 +0530

    Update SieveOfEratosthenes.py

--- Python/SieveOfEratosthenes.py
+++ Python/SieveOfEratosthenes.py
@@ -1,9 +1,11 @@
 n = 1800
 sieve = [1]*(n+1)
 for i in range(2,n+1):
-    if(sieve[i]==1):
-        for j in range(i,(n+2)//i):
-            sieve[i*j] = 0
+    if(sieve[i]):
+        # for j in range(i,(n+2)//i):
+        #     sieve[i*j] = 0
+        for j in range(2*i,n+1,i):
+            sieve[j] = 0
 for i in range(2,n+1):
     if sieve[i]==1:
         print(i, end=" ")
